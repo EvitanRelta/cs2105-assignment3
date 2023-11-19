6b356ee93f6f8242a6ac2d63f37e0400970e8fbc
EvitanRelta <shauntanzongzhi@gmail.com>
2023-11-19 19:55:53 +0800

    Draft 1

--- Integrity-A0235143N.py
+++ Integrity-A0235143N.py
@@ -4,6 +4,13 @@ import sys
 
 from Cryptodome.Hash import SHA256
 
+
+def generate_mac(key: bytes, data: bytes) -> bytes:
+    h = SHA256.new(data)
+    h.update(key)
+    return h.digest()
+
+
 if len(sys.argv) < 3:
     print("Usage: python3 ", os.path.basename(__file__), "key_file_name document_file_name")
     sys.exit()
@@ -12,17 +19,19 @@ key_file_name = sys.argv[1]
 file_name = sys.argv[2]
 
 # get the authentication key from the file
-# TODO
+with open(key_file_name, "rb") as key_file:
+    key = key_file.read()
 
 # read the input file
+with open(file_name, "rb") as file:
+    file_content = file.read()
 
 # First 32 bytes is the message authentication code
-# TODO
-mac_from_file = "get me from the file"
+mac_from_file = file_content[:32]
 
 # Use the remaining file content to generate the message authentication code
-# TODO
-mac_generated = "generated from the file"
+data = file_content[32:]
+mac_generated = generate_mac(key, data)
 
 if mac_from_file == mac_generated:
     print("yes")
